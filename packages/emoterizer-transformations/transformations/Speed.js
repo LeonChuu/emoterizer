93f09fa966f9650a234645b050bc4852e70cea50
LeonChuu <49106034+LeonChuu@users.noreply.github.com>
2021-08-15 22:06:01 -0300

    feat: adding validation to Speed.js

--- packages/emoterizer-transformations/transformations/Speed.js
+++ packages/emoterizer-transformations/transformations/Speed.js
@@ -5,18 +5,20 @@ class Speed {
    * @param {Object} options  - optional parameters
    * @returns {PseudoGif}  transformed image.
    */
-  static async transform (gif, options) {
-    const speed = parseFloat(options.delay) || 1
+  static async transform (gif, { delay }) {
+    const delayInRange = (parseFloat(delay) || 1) + 1
     const outputFrameList = []
     gif.frames.forEach(frame => {
-      frame.delayCentisecs = speed
+      frame.delayCentisecs = delayInRange
       outputFrameList.push(frame)
     })
     return new PseudoGif(outputFrameList, gif.height, gif.width)
   }
 
-  static async helpText () {
-    return 'Speeds up an gif.\n Options:\n delay ( 2 to 100) - delay between frames in centisecs.'
+  static validateDelay (delay) {
+    if ((delay < 1) || (delay > 100)) {
+      throw RangeError('Speed must be between 1 and 100')
+    }
   }
 }
 
